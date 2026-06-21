40a17abe907e7ede058f052bfdb4de84b3881169
khammersmith17 <kilianhammersmith@gmail.com>
2026-06-21 18:50:14 -0400

    batman

---
+++ src/sstable/bloom_filter.rs
@@ -0,0 +1,106 @@
+use crate::util;
+use std::hash::{DefaultHasher, Hash, Hasher};
+
+fn get_be_array(buffer: &[u8], offset: usize) -> [u8; 8] {
+    buffer[offset..offset + 8]
+        .try_into()
+        .expect("Invalid size slice when deserializing bloom filter")
+}
+
+fn hash1(key: &str) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    0_u64.hash(&mut hasher);
+    hasher.write(key.as_bytes());
+    hasher.finish()
+}
+fn hash2(key: &str) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    1_u64.hash(&mut hasher);
+    hasher.write(key.as_bytes());
+    hasher.finish()
+}
+
+pub struct BloomFilter {
+    bits: Vec<u64>,
+    num_bits: u64,
+    num_hashes: u64,
+}
+
+impl BloomFilter {
+    pub fn new(size: usize) -> BloomFilter {
+        let num_bits = size * 10;
+        let num_words = (num_bits + 63) / 64;
+        let bits = vec![0_u64; num_words];
+        let num_bits = 64 * bits.len() as u64;
+        let num_hashes =
+            (((num_bits as f64 / size as f64) * std::f64::consts::LN_2).round() as u64).max(1);
+
+        BloomFilter {
+            bits,
+            num_hashes,
+            num_bits,
+        }
+    }
+
+    pub fn from_bytes(buffer: &[u8]) -> BloomFilter {
+        let (num_words, bytes_walked) = util::decode_varint(buffer, 0_usize);
+        let mut offset = bytes_walked;
+        let mut bits: Vec<u64> = Vec::with_capacity(num_words as usize);
+        for _ in 0..num_words {
+            let be_bytes: [u8; 8] = get_be_array(buffer, offset);
+            bits.push(u64::from_be_bytes(be_bytes));
+            offset += 8;
+        }
+
+        let num_bits = 64_u64 * bits.len() as u64;
+        let be_bytes: [u8; 8] = get_be_array(buffer, offset);
+        let num_hashes = u64::from_be_bytes(be_bytes);
+
+        BloomFilter {
+            bits,
+            num_bits,
+            num_hashes,
+        }
+    }
+
+    pub fn insert(&mut self, key: &str) {
+        let h1 = hash1(key);
+        let h2 = hash2(key);
+
+        for i in 0..self.num_hashes {
+            let bit_idx = h1.wrapping_add((i as u64).wrapping_mul(h2)) % self.num_bits;
+            let word_idx = (bit_idx / 64) as usize;
+            let bit_offset = (bit_idx % 64) as usize;
+            self.bits[word_idx] |= 1 << bit_offset;
+        }
+    }
+
+    pub fn contains(&self, key: &str) -> bool {
+        let h1 = hash1(key);
+        let h2 = hash2(key);
+
+        for i in 0..self.num_hashes {
+            let bit_idx = h1.wrapping_add((i as u64).wrapping_mul(h2)) % self.num_bits;
+            let word_idx = (bit_idx / 64) as usize;
+            let bit_offset = (bit_idx % 64) as usize;
+            if self.bits[word_idx] & (1 << bit_offset) == 0 {
+                return false;
+            };
+        }
+        true
+    }
+
+    pub fn serialize(self) -> Vec<u8> {
+        let (bit_len_varint, varint_size) = util::encode_varint(self.bits.len());
+        let buffer_size = varint_size + (8 * (self.bits.len() + 1));
+        let mut buffer = vec![0_u8; buffer_size];
+        buffer[..varint_size].copy_from_slice(&bit_len_varint[..varint_size]);
+        let mut offset = varint_size;
+        for i in 0..self.bits.len() {
+            buffer[offset..offset + 8].copy_from_slice(&self.bits[i].to_be_bytes());
+            offset += 8;
+        }
+        buffer[offset..offset + 8].copy_from_slice(&self.num_hashes.to_be_bytes());
+        buffer
+    }
+}
---
+++ src/sstable/encode.rs
@@ -0,0 +1,115 @@
+use crate::constants;
+use crate::disk::encode;
+use crate::memtable::inner::MemtableInner;
+use crate::sstable::bloom_filter::BloomFilter;
+use crate::util;
+use std::fs::File;
+use std::io::Write;
+
+fn encode_index_block(index_block: Vec<(String, u64)>) -> Vec<u8> {
+    let buffer_size: usize = index_block.iter().map(|(key, _)| key.len() + 8 + 2).sum();
+    let mut buffer = vec![0_u8; buffer_size];
+    let mut offset = 0_usize;
+
+    index_block.iter().for_each(|(key, key_offset)| {
+        let (key_len_varint, varint_len) = util::encode_varint(key.len());
+
+        buffer[offset..offset + varint_len].copy_from_slice(&key_len_varint[..varint_len]);
+        offset += varint_len;
+        buffer[offset..offset + key.len()].copy_from_slice(&key.as_bytes());
+        offset += key.len();
+        buffer[offset..offset + 8].copy_from_slice(&key_offset.to_be_bytes());
+        offset += 8;
+    });
+    buffer[..offset].to_vec()
+}
+
+fn encode_footer(data_block_end: u64, index_block_end: u64, bloom_filter_end: u64) -> Vec<u8> {
+    let mut buffer = vec![0_u8; 24];
+    buffer[..8].copy_from_slice(&data_block_end.to_be_bytes());
+    buffer[8..16].copy_from_slice(&index_block_end.to_be_bytes());
+    buffer[16..].copy_from_slice(&bloom_filter_end.to_be_bytes());
+    buffer
+}
+
+pub fn write_sstable(table: &MemtableInner, fd: &mut File) -> std::io::Result<()> {
+    /*
+     * Allocate an entire buffer of total size + number of records * 4 for variants.
+     * DFS inorder to insert records in sorted order
+     * */
+    let mut disk_size = 0_usize;
+    let mut index_block: Vec<(String, u64)> =
+        Vec::with_capacity(table.current_size / constants::DISK_BLOCK_SIZE);
+    let mut bloom_filter = BloomFilter::new(table.arena.len());
+    inorder_flush(
+        table,
+        fd,
+        table.root_node,
+        &mut disk_size,
+        &mut index_block,
+        &mut bloom_filter,
+    )?;
+
+    let data_block_end = disk_size;
+    let index_block_buffer = encode_index_block(index_block);
+    let index_block_end = data_block_end + index_block_buffer.len();
+    fd.write(&index_block_buffer)?;
+    let bloom_filter_buffer = bloom_filter.serialize();
+    let bloom_filter_end = index_block_end + bloom_filter_buffer.len();
+    fd.write(&bloom_filter_buffer)?;
+    let footer = encode_footer(
+        data_block_end as u64,
+        index_block_end as u64,
+        bloom_filter_end as u64,
+    );
+    fd.write(&footer)?;
+    fd.flush()?;
+    Ok(())
+}
+
+fn inorder_flush(
+    table: &MemtableInner,
+    fd: &mut File,
+    node_idx_opt: Option<usize>,
+    disk_size: &mut usize,
+    index_block: &mut Vec<(String, u64)>,
+    bloom_filter: &mut BloomFilter,
+) -> std::io::Result<()> {
+    let Some(node_idx) = node_idx_opt else {
+        return Ok(());
+    };
+
+    inorder_flush(
+        table,
+        fd,
+        table.arena[node_idx].left,
+        disk_size,
+        index_block,
+        bloom_filter,
+    )?;
+    {
+        // flush current node
+        let current_node = &table.arena[node_idx];
+        let before = *disk_size % constants::DISK_BLOCK_SIZE;
+        let record_offset = *disk_size;
+        let disk_record = encode::encode_record(current_node);
+        bloom_filter.insert(current_node.key.as_str());
+        *disk_size += disk_record.len();
+        let after = *disk_size % constants::DISK_BLOCK_SIZE;
+
+        if before > after || record_offset == 0 {
+            index_block.push((current_node.key.clone(), record_offset as u64));
+        }
+        fd.write(&disk_record)?;
+    }
+    inorder_flush(
+        table,
+        fd,
+        table.arena[node_idx].right,
+        disk_size,
+        index_block,
+        bloom_filter,
+    )?;
+
+    Ok(())
+}
