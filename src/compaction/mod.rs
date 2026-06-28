f55ab100617ea30c8d7eeb68e1f0daf7583dc29f
khammersmith17 <kilianhammersmith@gmail.com>
2026-06-28 14:59:27 -0400

    compaction scaffold

---
+++ src/compaction/mod.rs
@@ -0,0 +1,15 @@
+use crate::sstable::SSTableCache;
+use tokio::sync::mpsc::Receiver;
+/*
+* Waits for signal from db thread.
+*
+* on signal:
+*   get a reference to all tables.
+*   while there are tables to merge.
+* */
+pub async fn compaction_fn(receiver: Receiver<u8>, sstable_cache: SSTableCache) {
+    while let Some(_singal) = self.receiver.recv().await {
+        let tables = sstable_cache.clone_tables();
+        todo!()
+    }
+}
